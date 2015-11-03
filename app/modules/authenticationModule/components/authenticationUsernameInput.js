1950728fe2518e1d1621aeeda2390902bee1e9b8
spanias <demetris@spanias.com>
2015-11-03 17:43:30 +0200

    Building Create User Page

---
+++ app/modules/authenticationModule/components/authenticationUsernameInput.js
@@ -0,0 +1,94 @@
+/**
+ * Copyright 2015, Digital Optimization Group, LLC.
+ * Copyrights licensed under the APACHE 2 License. See the accompanying LICENSE file for terms.
+ */
+
+import React from 'react';
+import {connectToStores} from 'fluxible-addons-react';
+import {Button, Input} from 'react-bootstrap';
+import {Label} from 'react-bootstrap';
+import AuthenticationActions  from '../actions/authenticationActions';
+
+var debug = require('debug')('AuthenticationUsernameInput');
+
+class AuthenticationUsernameInput extends React.Component {
+
+    constructor(props, context) {
+        super(props,context);
+        this.state = {
+            username: ""
+        };
+
+        this._refreshStateWithProps = this._refreshStateWithProps.bind(this);
+        this._handleUsernameInput = this._handleUsernameInput.bind(this);
+        this.hasChanges = this.hasChanges.bind(this);
+        this.isValid = this.isValid.bind(this);
+        this.getValue = this.getValue.bind(this);
+    }
+    componentDidMount() {
+        if (this.props != null) {
+            debug("didMount ->", this.props);
+            this._refreshStateWithProps(this.props);
+        }
+    }
+
+    _refreshStateWithProps(nextProps) {
+        if (typeof nextProps !== "undefined") {
+            this.setState({
+                username: nextProps.username
+            });
+        }
+        else {
+            this.setState({
+                username:""
+            });
+        }
+    }
+
+    _handleUsernameInput() {
+        this.setState({
+            username: this.refs.username.getValue()
+        });
+
+        if (this.props.onChange) {
+            this.props.onChange();
+        }
+    }
+
+
+    hasChanges() {
+        return (this.state.username != this.props.username);
+    }
+
+    isValid(){
+        return (this.state.username != "");
+    }
+    getValue(){
+        return this.refs.username.getValue();
+    }
+
+
+    render() {
+        debug("Rendering");
+        return (
+            <div>
+                <Input
+                    type="text"
+                    placeholder="Enter text"
+                    label="Username"
+                    ref="username"
+                    value={this.state.username}
+                    defaultValue={this.props.username}
+                    onChange={this._handleUsernameInput}/>
+            </div>
+        );
+    }
+}
+
+AuthenticationUsernameInput.propTypes = {
+    username: React.PropTypes.string,
+    onChange: React.PropTypes.func
+
+};
+
+export default AuthenticationUsernameInput;
--- app/modules/authenticationModule/pages/createUser.js
+++ app/modules/authenticationModule/pages/createUser.js
@@ -5,9 +5,14 @@
 
 import React from 'react';
 import {connectToStores} from 'fluxible-addons-react';
-import {ButtonToolbar, Modal, Button, Input, Row, Col, Alert, ModalTrigger} from 'react-bootstrap';
+import { Panel, Button, Input, Row, Col} from 'react-bootstrap';
 import AuthenticationActions  from '../actions/authenticationActions';
 import AuthenticationMainStore from '../stores/authenticationMainStore';
+
+import AuthenticationFirstNameInput from "../components/authenticationFirstNameInput";
+import AuthenticationUsernameInput from "../components/authenticationUsernameInput";
+import AuthenticationLastNameInput from "../components/authenticationLastNameInput";
+import AuthenticationEmailInput from "../components/authenticationEmailInput";
 
 var debug = require('debug')('CreateUserPage');
 
@@ -17,10 +22,62 @@ class CreateUserPage extends React.Component {
     }
 
     render() {
+        var createUserForm = '';
+        if (!this.props.loggedIn)
+        {
+            createUserForm=  <div className="authentication-createUser-group">
+                <Panel header="User Information" bsStyle="primary">
+                    <Row>
+                        <Col xs={6}>
+                            <AuthenticationUsernameInput
+                                ref="AuthenticationUsernameInput"
+                                username={this.props.username}
+                                onChange={this._handleUsernameInput}
+                                />
+                        </Col>
+                        <Col xs={6}>
+                            <AuthenticationEmailInput
+                                ref="AuthenticationEmailInput"
+                                email={this.props.email}
+                                verified={this.props.verified}
+                                requestValidationEmail={this._requestValidationEmail}
+                                onChange={this._handleEmailChange}
+                                />
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col xs={12}>
+
+                        </Col>
+                    </Row>
+
+                    <Row>
+                        <Col xs={6}>
+                            <AuthenticationFirstNameInput
+                                ref="AuthenticationFirstNameInput"
+                                firstName={this.props.firstName}
+                                onChange={this._handleFirstNameInput}
+                                />
+                        </Col>
+                        <Col xs={6}>
+                            <AuthenticationLastNameInput
+                                ref="AuthenticationLastNameInput"
+                                lastName={this.props.lastName}
+                                onChange={this._handleLastNameInput}/>
+                        </Col>
+                    </Row>
+                </Panel>
+            </div>;
+
+        }
+
         return (
             <div className="container">
                 <h1> Create User Page! </h1>
+                {createUserForm}
             </div>
+
         );
     }
 }
